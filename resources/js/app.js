0f56b3bd6126362d380192e3a15c109bdd41f895
Azarel Grahandito <skootydack@gmail.com>
2024-10-21 23:07:06 +0700

    style: opinionated swiper view

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,11 @@ import Swiper from "swiper/bundle";
 import "swiper/css/bundle";
 
 document.addEventListener("DOMContentLoaded", function () {
-    const swiper = new Swiper(".multiple-slide-carousel", {
+    const swiper = new Swiper(".swiper-container", {
         loop: true,
-        slidesPerView: 6, // Adjusts the number of slides shown based on container width
+        slidesPerView: 5, // Adjusts the number of slides shown based on container width
         centeredSlides: true,
-        spaceBetween: 10, // Adjust this value for space between slides
+        spaceBetween: 5, // Adjust this value for space between slides
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
